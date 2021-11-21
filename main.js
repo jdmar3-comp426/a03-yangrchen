6b95a235980e5a29631edd685fbdaf4d62262b24
Yang Chen <yyangc88@gmail.com>
2021-11-20 21:13:45 -0500

    spicy_9 commit

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 import { sumToString, getIncreasingArray, countArray } from "./src/mild/mild_1.js";
 import * as mild_2 from "./src/mild/mild_2.js";
+import * as spicy_9 from "./src/spicy/spicy_9.js";
 
-console.log(sumToString(2, 4));
-console.log(getIncreasingArray(3, 7));
-console.log(countArray([3, 6, 3, 2, 2, 3, 'some', 'hello', 'some', [1, 2]]));
-console.log(mild_2.identifyVariable(2));
-console.log(mild_2.identifyArray(['some', 3, [3, 4], false]));
-let obj = { type: 'test' };
-mild_2.removeKey(obj, 'type');
-console.log({ obj });
-let obj_2 = { type: 'test2', a: 'a', };
-obj_2 = mild_2.removeKeyNonDestructive(obj_2, 'type');
-console.log(obj_2);
-let obj_3 = { type: 'test3', a: 'a', b: 'b', };
-obj_3 = mild_2.removeKeys(obj_3, ['a', 'b'])
-console.log({ obj_3 });
+console.log(spicy_9.repeat((a, b) => a + b, 5, 1, 2))
+console.log(spicy_9.repeatDemo());
+const multiplyBy2 = spicy_9.multiplyBy(2);
+console.log(multiplyBy2(8));
+
+console.log(spicy_9.everyEven([], x => x == 1));
+console.log(spicy_9.someEven([3, 3, 2, 1, 0], x => x === 3));
+console.log(spicy_9.filter([1, 2, 3], x => x % 2 == 0));
+console.log(spicy_9.allEvensAreOdd([1, 2, 3, 4, 6]));
+console.log(spicy_9.hasExactly(['a', 'b', 'c'], x => typeof (x) == 'string', 3));
--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -15,7 +15,12 @@
  *                from calling the function
  */
 export const repeat = (fn, n, ...params) => {
-
+    let res = [];
+    while (n > 0) {
+        res.push(fn(...params));
+        n -= 1;
+    }
+    return res;
 };
 
 
@@ -24,7 +29,7 @@ export const repeat = (fn, n, ...params) => {
  *   10 times.
  */
 export const repeatDemo = () => {
-
+    repeat(a => console.log(a), 10, "Hello, world!");
 };
 
 
@@ -41,7 +46,10 @@ export const repeatDemo = () => {
  *   product of num1 and num2.
  */
 export const multiplyBy = (num1) => {
-
+    const funcA = num2 => {
+        return num1 * num2;
+    }
+    return funcA;
 };
 
 
@@ -49,7 +57,7 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
@@ -57,7 +65,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
@@ -85,7 +93,15 @@ export const tenTimesFifty = () => {
  *    everyEven([1, 1, 0, 1, 1], x => x === 1)  <--  returns false
  */
 export const everyEven = (arr, test) => {
-
+    if (arr.length === 0) {
+        return false;
+    }
+    for (let i = 0; i < arr.length; i += 2) {
+        if (!test(arr[i])) {
+            return false;
+        }
+    }
+    return true;
 };
 
 
@@ -109,7 +125,12 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-
+    for (let i = 0; i < arr.length; i += 2) {
+        if (test(arr[i])) {
+            return true;
+        }
+    }
+    return false;
 };
 
 
@@ -135,7 +156,15 @@ export const someEven = (arr, test) => {
  *       -->  { pass: [1, 5, 31], fail: [90] }
  */
 export const filter = (arr, test) => {
-
+    let d = { fail: [], pass: [] };
+    arr.forEach(ele => {
+        if (test(ele)) {
+            d['pass'].push(ele);
+        } else {
+            d['fail'].push(ele);
+        }
+    });
+    return d;
 };
 
 
@@ -145,7 +174,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x => x % 2 == 1);
 };
 
 
@@ -155,7 +184,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x => x % 2 == 1);
 };
 
 
@@ -166,5 +195,6 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    let d = filter(arr, test);
+    return n == d['pass'].length;
 };
